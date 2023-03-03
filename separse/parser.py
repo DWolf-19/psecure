fbaedfc2c3c12189254cb463eb591a90eec1fcbf
DWolf_19 <leoconnect.o@gmail.com>
2023-03-04 00:16:30 +0300

    Clear the expression of spaces

--- separse/parser.py
+++ separse/parser.py
@@ -1,6 +1,6 @@
 class Parser:
     def __init__(self, expression: str):
-        self.expression = expression
+        self.expression = expression.replace(" ", "")
 
     def parse(self, *args) -> int | float:
         pass
