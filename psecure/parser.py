8c5544ef9ca15958e9b00fccc62ec38979f3564a
DWolf_19 <leoconnect.o@gmail.com>
2023-03-04 14:35:14 +0300

    Add math constants

--- psecure/parser.py
+++ psecure/parser.py
@@ -1,3 +1,17 @@
+import math
+import enum
+
+import scipy
+
+
+class Constants(enum.Enum):
+    MATH_CONSTANTS = {
+        "PI": math.pi,
+        "E": math.e,
+        # Golden ratio
+        "GOLDEN": scipy.constants.golden
+    }
+
 class Parser:
     def __init__(self, expression: str):
         self.expression = expression.replace(" ", "")
