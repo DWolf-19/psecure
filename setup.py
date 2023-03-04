f718d07f97bbf4aacafd008ef2da978f511c0d80
DWolf_19 <leoconnect.o@gmail.com>
2023-03-04 14:04:43 +0300

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup
+import setuptools
 
 def get_long_description() -> str:
     with open("README.md") as file:
         return file.read()
 
-setup(
+def parse_requirements() -> list[str]:
+    with open("requirements.txt") as file:
+        dependencies = (d.strip() for d in file.read().split("\n") if d.strip())
+        return [d for d in dependencies if not d.startswith("#")]
+
+setuptools.setup(
     name="psecure",
     description="A secure parser of math expressions from str.",
     long_description=get_long_description(),
@@ -13,5 +18,6 @@ setup(
     author="DWolf_19",
     license="MIT License",
     url="https://github.com/DWolf-19/psecure",
+    install_requires=parse_requirements(),
     packages=["psecure"],
 )
